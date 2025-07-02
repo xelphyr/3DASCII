0538cb637a3c10a23e927d41b6d9ca733d265aed
xelphyr <xelphyrnull@gmail.com>
2025-07-02 22:30:34 +0530

    made the -static-libxx lines run only if on windows

--- release/_deps/sdl3-build/include-revision/SDL3/SDL_revision.h
+++ release/_deps/sdl3-build/include-revision/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL3-3.3.0-2606b080 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL3-3.3.0-530639aa (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL3-3.3.0-2606b080"
+#define SDL_REVISION "SDL3-3.3.0-530639aa"
 #endif
 
 #endif /* SDL_revision_h_ */
